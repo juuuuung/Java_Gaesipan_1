a4cb7c0a7f2fcf5afdc32c7a0a5a2e896b9cae4c
juuuuung <pandanam987@gmail.com>
2022-07-16 20:54:42 +0900

    내가 새로 만들어본 게시판

--- src/java_게시판_2/Main.java
+++ src/java_게시판_2/Main.java
@@ -1,17 +1,23 @@
 package java_게시판_2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		String[] datas = new String[100];
-		int lastIndex = 0; // datas의 마지막 인덱스를 확인 하기 위한 변수
+
+		List<urticle> Articles = new ArrayList<>();
+
+		int TextIndex = 0;
 		System.out.println("=======프로그램 시작====");
+
 		Scanner s = new Scanner(System.in);
 		while (true) { // 명령어 반복
 			System.out.printf("명령어 입력 : ");
 			String command = s.nextLine(); // Scanner class
+
 			command = command.trim(); // 공백제거후 command에 저장
 
 			if (command.length() == 0) { // 만약 명령어 입력 하지 않고 enter 쳤을때 continue
@@ -21,22 +27,31 @@ public class Main {
 			if (command.equals("exit")) { // exit입력 하면 break ? equals 뭔지 모름ss
 				break;
 			} else if (command.equals("article list")) { // article list 출력
-				if (lastIndex != 0) {
-					for (int i = 0; i < lastIndex; i++) { // datas에 저장된것들 출력
-						System.out.printf("%d번 %s\n", i + 1, datas[i]);
-					}
-				} else
+				if(Articles.size() == 0){
+					System.out.println("게시물이 없습니다.");
 					continue;
+				} else{
+					for(int i=Articles.size()-1; i >= 0; i--){
+						urticle article = Articles.get(i);
+						System.out.printf("번호 : %d\n제목 : %s\n", article.TextNum, article.TextTitle);
+					}
+				}
 			} else if (command.equals("article write")) { // 게시글 작성
+				TextIndex++;
 
-				System.out.printf("제목 입력 : ");
-				datas[lastIndex] = s.nextLine();
-				System.out.printf("내용 입력 : ");
-				String article = s.nextLine();
-				System.out.println((lastIndex + 1) + "번 글 제목 : " + datas[lastIndex] + " 내용 : " + article);
-				lastIndex++;
+				System.out.printf("제목 : ");
+				String TextTitle = s.nextLine();
 
-			} else {
+				System.out.printf("내용 : ");
+				String TextArticle = s.nextLine();
+
+				urticle NewArticle = new urticle(TextIndex, TextTitle, TextArticle);
+				Articles.add(NewArticle);
+				
+
+			} else if(command.startsWith("article detail")){
+
+			}else {
 				System.out.println("명령어 없음");
 			}
 		}
@@ -44,4 +59,16 @@ public class Main {
 		System.out.println("=====프로그램 끝=========");
 	}
 
+}
+
+class urticle{
+	int TextNum;
+	String TextTitle;
+	String TextArticle;
+
+	public urticle(int TextNum, String TextTitle, String TextArticle){
+		this.TextNum = TextNum;
+		this.TextTitle = TextTitle;
+		this.TextArticle = TextArticle;
+	}
 }
