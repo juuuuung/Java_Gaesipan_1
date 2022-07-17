ae78460c8d78d56934156abf991baa9084639570
juuuuung <pandanam987@gmail.com>
2022-07-17 11:53:05 +0900

    Main.java 복습용으로 다시 만듬 (삭제, 날짜, 디테일)

--- src/java_게시판_2/Main.java
+++ src/java_게시판_2/Main.java
@@ -1,5 +1,8 @@
 package java_게시판_2;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -8,7 +11,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		List<urticle> Articles = new ArrayList<>();
+		List<article> Articles = new ArrayList<>();
 
 		int TextIndex = 0;
 		System.out.println("=======프로그램 시작====");
@@ -27,12 +30,12 @@ public class Main {
 			if (command.equals("exit")) { // exit입력 하면 break ? equals 뭔지 모름ss
 				break;
 			} else if (command.equals("article list")) { // article list 출력
-				if(Articles.size() == 0){
+				if (Articles.size() == 0) {
 					System.out.println("게시물이 없습니다.");
 					continue;
-				} else{
-					for(int i=Articles.size()-1; i >= 0; i--){
-						urticle article = Articles.get(i);
+				} else {
+					for (int i = Articles.size() - 1; i >= 0; i--) {
+						article article = Articles.get(i);
 						System.out.printf("번호 : %d\n제목 : %s\n", article.TextNum, article.TextTitle);
 					}
 				}
@@ -45,13 +48,66 @@ public class Main {
 				System.out.printf("내용 : ");
 				String TextArticle = s.nextLine();
 
-				urticle NewArticle = new urticle(TextIndex, TextTitle, TextArticle);
+				LocalDateTime now = LocalDateTime.now();
+
+				String FormatedNow = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss"));
+
+				article NewArticle = new article(TextIndex, TextTitle, TextArticle, FormatedNow);
 				Articles.add(NewArticle);
+
+			} else if (command.startsWith("article detail")) {
+				String[] SplitCommand = command.split(" ");
+				int id = Integer.parseInt(SplitCommand[2]);
+
+				if (id > Articles.size()) {
+					System.out.println("게시글이 없음");
+				} else {
+					article newarticle = Articles.get(id - 1);
+					System.out.printf("번호 : %d\n제목 : %s\n내용 : %s\n날짜 : %s\n", newarticle.TextNum, newarticle.TextTitle,
+							newarticle.TextArticle, newarticle.FormatedNow);
+				}
+
+			} else if (command.startsWith("article delete")) {
+				String[] SplitCommand = command.split(" ");
+				int id = Integer.parseInt(SplitCommand[2]);
 				
+				//<==============방법 1==============>
+				int foundIndex = -1;
+				
+				for(int i=0; i < Articles.size(); i++) {
+					article newarticle = Articles.get(i);
+					if(newarticle.TextNum == id) {
+						foundIndex = i;
+						break;
+					}
+				}
+				if(foundIndex == -1) {
+					System.out.printf("%d 존재 X", id);
+				}
+				Articles.remove(foundIndex);
+				System.out.printf("%d 게시물 삭제 완료", id);
 
-			} else if(command.startsWith("article detail")){
-
-			}else {
+				//<==============방법 2===================>
+				/*if (id > Articles.size()) {
+					System.out.println("게시물이 없음");
+				} else {
+					Articles.remove(id - 1);
+					// 앞으로 땡겨줌 글 번호를
+					for (int i = 0; i < Articles.size(); i++) {
+						Articles.get(i).TextNum = i + 1;
+					}
+					System.out.printf("%d번 게시글이 삭제되었습니다\n", id);
+					TextIndex--;
+				}*/
+			} else if (command.startsWith("article correction")) {
+				String[] SplitCommand = command.split(" ");
+				int id = Integer.parseInt(SplitCommand[2]);
+				
+				article newarticle = Articles.get(id-1);
+				
+				System.out.println("제목 : ");
+				String Text
+			} else {
 				System.out.println("명령어 없음");
 			}
 		}
@@ -61,14 +117,16 @@ public class Main {
 
 }
 
-class urticle{
+class article {
 	int TextNum;
 	String TextTitle;
 	String TextArticle;
+	String FormatedNow;
 
-	public urticle(int TextNum, String TextTitle, String TextArticle){
+	public article(int TextNum, String TextTitle, String TextArticle, String FormatedNow) {
 		this.TextNum = TextNum;
 		this.TextTitle = TextTitle;
 		this.TextArticle = TextArticle;
+		this.FormatedNow = FormatedNow;
 	}
 }
